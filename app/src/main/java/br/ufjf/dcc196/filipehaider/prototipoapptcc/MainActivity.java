e7a04d57556759115da55520b1e98383d16a05fe
filipehaider <ovitiguf@gmail.com>
2021-09-26 15:55:34 -0300

    Adiciona mudança de texto na tela Main através da escolha dos botões

--- app/src/main/java/br/ufjf/dcc196/filipehaider/prototipoapptcc/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/filipehaider/prototipoapptcc/MainActivity.java
@@ -3,12 +3,48 @@ package br.ufjf.dcc196.filipehaider.prototipoapptcc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonOp1, buttonOp2, buttonOp3;
+    TextView textViewTexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonOp1 = findViewById(R.id.buttonOp1);
+        buttonOp2 = findViewById(R.id.buttonOp2);
+        buttonOp3 = findViewById(R.id.buttonOp3);
+        textViewTexto = findViewById(R.id.textViewTexto);
+
+
+        buttonOp1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textViewTexto.setText("Voce escolheu a opção 1");
+            }
+        });
+
+        buttonOp2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textViewTexto.setText("Parabéns o caminho escolhido foi o 2");
+            }
+        });
+
+        buttonOp3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textViewTexto.setText("Caminho de número 3");
+            }
+        });
+
+
+
     }
 }
