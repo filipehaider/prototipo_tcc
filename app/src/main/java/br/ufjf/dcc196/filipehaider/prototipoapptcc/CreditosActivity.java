167196b8a58df45d82a1f5e3fa9acf5e492b7fbd
filipehaider <ovitiguf@gmail.com>
2021-09-26 10:18:38 -0300

    Adiciona Créditos

--- app/src/main/java/br/ufjf/dcc196/filipehaider/prototipoapptcc/CreditosActivity.java
+++ app/src/main/java/br/ufjf/dcc196/filipehaider/prototipoapptcc/CreditosActivity.java
@@ -2,13 +2,27 @@ package br.ufjf.dcc196.filipehaider.prototipoapptcc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreditosActivity extends AppCompatActivity {
+    Button buttonVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creditos);
+
+        buttonVoltar = findViewById(R.id.buttonVoltar);
+
+        buttonVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(CreditosActivity.this, ApresentacaoActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
