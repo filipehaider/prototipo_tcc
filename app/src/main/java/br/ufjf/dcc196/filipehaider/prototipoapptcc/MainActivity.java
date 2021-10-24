dde24a8630165918a08baafb74368df5346b5fcd
filipehaider <ovitiguf@gmail.com>
2021-10-23 21:49:55 -0300

    Adiciona inky na Activty

--- app/src/main/java/br/ufjf/dcc196/filipehaider/prototipoapptcc/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/filipehaider/prototipoapptcc/MainActivity.java
@@ -7,10 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.bladecoder.ink.runtime.Choice;
+import com.bladecoder.ink.runtime.Story;
+
 public class MainActivity extends AppCompatActivity {
 
     Button buttonOp1, buttonOp2, buttonOp3;
     TextView textViewTexto;
+    Story story;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +25,24 @@ public class MainActivity extends AppCompatActivity {
         buttonOp2 = findViewById(R.id.buttonOp2);
         buttonOp3 = findViewById(R.id.buttonOp3);
         textViewTexto = findViewById(R.id.textViewTexto);
+        String  sourceJsonString = getString(R.string.historia_1);
+        story = null;
+        try {
+            story = new Story(sourceJsonString);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
         buttonOp1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 textViewTexto.setText("Voce escolheu a opção 1");
-                //story.chooseChoiceIndex(0);
+                try {
+                    story.chooseChoiceIndex(0);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -35,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 textViewTexto.setText("Parabéns o caminho escolhido foi o 2");
-                //story.chooseChoiceIndex(0);
+                //story.chooseChoiceIndex(1);
             }
         });
 
@@ -43,11 +58,34 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 textViewTexto.setText("Caminho de número 3");
-                //story.chooseChoiceIndex(0);
+                //story.chooseChoiceIndex(2);
             }
         });
+    }
+
+    public void atualizaTela(){
+        try {
+        // 2) Game content, line by line
+        while (story.canContinue()) {
+            String line = null;
+                line = story.Continue();
+                // Carregar o trecho do texto
 
 
+// 3) Display story.currentChoices list, allow player to choose one
+        if (story.getCurrentChoices().size() > 0) {
+            for (Choice c : story.getCurrentChoices()) {
+                System.out.println(c.getText());
+                // Carregar o texto dos botoes
+            }
+
+
+        } }
+        } catch (Exception e) {
+                e.printStackTrace();
+            }
 
     }
+
+
 }
